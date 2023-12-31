e9af2fdfff3c9ec529030b685d6fbf34fcba7f98
Mohammad Hosseini <mohammad.hoseyny@gmail.com>
2024-01-01 01:10:25 +0330

    feat: change the algorithm to static retrieval

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from ortools.linear_solver import pywraplp
 import pickle
 
 
@@ -7,75 +6,47 @@ app = Flask(__name__)
 
 ISP_PACKAGES = {}  
 
-isp_data_paths = {
-    'MCI - همراه‌اول': 'data/mci.pk',
-    'MTN - ایرانسل': 'data/mtn.pk'
+isp_name_mapper = {
+    'mci': 'MCI - همراه‌اول',
+    'mtn': 'MTN - ایرانسل'
 }
 
+
+def get_isp_name(isp):
+    if isp in isp_name_mapper:
+        return isp_name_mapper[isp]
+    return isp
+
+def get_isp_reverse_name(isp_name):
+    for isp, name in isp_name_mapper.items():
+        if isp_name == name:
+            return isp
+
 def load_isp_data():
-    print('Reading the isps')
-    for isp, path in isp_data_paths.items():
-        with open(path, 'rb') as f:
-            ISP_PACKAGES[isp] = pickle.load(f)
-    print(f'The ISPs {len(ISP_PACKAGES)}')
+    global ISP_PACKAGES
+    with open('data/data.pk', 'rb') as f:
+        ISP_PACKAGES = pickle.load(f)
     
-def calculator(usage, duration, isp):
-    solver = pywraplp.Solver.CreateSolver("SCIP")
-    
-    packages = ISP_PACKAGES[isp]
-
-    # Define the decision variables
-    x = [solver.IntVar(0, solver.infinity(), i['name']) for i in packages]
-
-    # Define the objective function
-    solver.Minimize(solver.Sum([x[i] * packages[i]["price"] for i in range(len(packages))]))
-
-    # Define the constraints
-    solver.Add(solver.Sum([x[i] * packages[i]["volume"] for i in range(len(packages))]) >= usage*1024)
-    solver.Add(solver.Sum([x[i] * packages[i]["duration"] for i in range(len(packages))]) >= duration)
-
-    # Solve the problem
-    status = solver.Solve()
-
-    # Print the results
-    if status == pywraplp.Solver.OPTIMAL:
-        print("The optimal solution is:")
-        result = {'total_price': solver.Objective().Value(), 'packages': []}
-        total_purchase = 0
-        total_volume = 0
-        for i in range(len(packages)):
-            if x[i].solution_value() > 0:
-                chosen_package = packages[i].copy()
-                chosen_package.update({'count': x[i].solution_value()})
-                total_purchase += x[i].solution_value()
-                total_volume += (packages[i]['volume'] * x[i].solution_value())
-                # print(f"  {x[i].solution_value()} package(s) of type {packages[i]['name']}")
-                result['packages'].append(chosen_package)
-        result.update({'total_purchase': total_purchase, 'total_volume': total_volume})
-        # print(f"The total cost is {solver.Objective().Value()} for {usage} GB")
-        # print("Per GB: {}".format(solver.Objective().Value()/usage))
-    else:
-        result = {'total_price': None, 'packages': []}
-        # print("The problem does not have an optimal solution.")
-    
-    
+def give_best_package_combination(usage, duration, isp):
+    result = ISP_PACKAGES[isp][usage]
     return result
 
 
 @app.route("/")
 def index():
-    isps = list(ISP_PACKAGES.keys())
+    isps = list(map(get_isp_name, isp_name_mapper.keys()))
     return render_template("index.html", isps=isps)
 
 
 @app.route("/calculate", methods=["POST"])
 def calculate():
     usage = int(request.form["usage"])
-    isp = request.form["isp"]
+    isp = get_isp_reverse_name(request.form["isp"])
+    if usage >= 1000:
+        return render_template("not_supported.html")
     
-    results = calculator(usage=usage, duration=28, isp=isp)
+    results = give_best_package_combination(usage=usage, duration=28, isp=isp)
     optimal_combination = results['packages']
-    print(optimal_combination)
     total_price = results['total_price']
     total_purchases = results['total_purchase']
     total_volume = float(results['total_volume'] / 1024)
--- data/pickle_creator.py
+++ data/pickle_creator.py
@@ -1,19 +1,96 @@
 import pandas as pd
 import pickle
+import os
+from ortools.linear_solver import pywraplp
 
-isps = {
-    'mtn': 'irancell.csv',
-    'mci': 'mci.csv'
-}
+TIME_INTERVAL = 28
+TOTAL_USAGE_LIMIT = 1000
 
-for isp, path in isps.items():
-    packages_df = pd.read_csv(path)
+def get_all_ips_csv_files():
+    directory = os.path.join('.')
+    files_path = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".csv"):
+                files_path.append(file)
+                
+    return files_path
+                
+
+def save_pickle(path, obj):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f)
+
+def get_calculated_df_isp(file_path):
+    packages_df = pd.read_csv(file_path)
     packages_df['price_per_gig'] = packages_df['price'] / (packages_df['volume'] / 1024)
     packages_df['price'] = packages_df['price'] * 1.09
 
     packages_df = packages_df[packages_df['is_nightly'] == False]
     packages = packages_df.to_dict('records')
 
+    return packages
 
-    with open(f'{isp}.pk', 'wb') as f:
-        pickle.dump(packages, f)
+def read_packages(files):
+    isp_packages = {}
+    for file in files:
+        isp_name = file.replace('.csv', '')
+        isp_packages[isp_name] = get_calculated_df_isp(file)
+    return isp_packages
+
+def best_package_combination_finder(usage, duration, packages):
+    solver = pywraplp.Solver.CreateSolver("SCIP")
+    
+
+    # Define the decision variables
+    x = [solver.IntVar(0, solver.infinity(), i['name']) for i in packages]
+
+    # Define the objective function
+    solver.Minimize(solver.Sum([x[i] * packages[i]["price"] for i in range(len(packages))]))
+
+    # Define the constraints
+    solver.Add(solver.Sum([x[i] * packages[i]["volume"] for i in range(len(packages))]) >= usage*1024)
+    solver.Add(solver.Sum([x[i] * packages[i]["duration"] for i in range(len(packages))]) >= duration)
+
+    # Solve the problem
+    status = solver.Solve()
+
+    # Print the results
+    if status == pywraplp.Solver.OPTIMAL:
+        result = {'total_price': solver.Objective().Value(), 'packages': []}
+        total_purchase = 0
+        total_volume = 0
+        for i in range(len(packages)):
+            if x[i].solution_value() > 0:
+                chosen_package = packages[i].copy()
+                chosen_package.update({'count': x[i].solution_value()})
+                total_purchase += x[i].solution_value()
+                total_volume += (packages[i]['volume'] * x[i].solution_value())
+                result['packages'].append(chosen_package)
+        result.update({'total_purchase': total_purchase, 'total_volume': total_volume})
+    else:
+        result = {'total_price': None, 'packages': []}
+    return result
+
+def best_packages_combination(isp_packages):
+    best_combinations = {}
+    for isp, packages in isp_packages.items():
+        if isp not in best_combinations:
+                best_combinations[isp] = {}
+        for usage in range(TOTAL_USAGE_LIMIT):
+            result = best_package_combination_finder(usage, TIME_INTERVAL, packages)
+            best_combinations[isp][usage] = result
+    return best_combinations
+
+if __name__ == '__main__':
+    print('Reading csv files')
+    files = get_all_ips_csv_files()
+    print(f'There are {len(files)} files in the directory')
+    print('Reading packages and calculate the price with tax and ...')
+    isp_packages = read_packages(files)
+    print('Finding the best package combination for different use usages')
+    best_combinations = best_packages_combination(isp_packages)
+    print('saving data.pk file for using in the web service')
+    save_pickle('data.pk', best_combinations)
+    
+    
